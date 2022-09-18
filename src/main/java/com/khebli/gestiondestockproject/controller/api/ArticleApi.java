bcfb92b0e73aaec939474937f4b14196fb60b8e3
meriemkhebli <am_khebli@esi.dz>
2022-09-18 13:31:38 +0100

    article with documentation swagger

--- src/main/java/com/khebli/gestiondestockproject/controller/api/ArticleApi.java
+++ src/main/java/com/khebli/gestiondestockproject/controller/api/ArticleApi.java
@@ -1,6 +1,10 @@
 package com.khebli.gestiondestockproject.controller.api;
 
 import com.khebli.gestiondestockproject.dto.ArticleDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,19 +12,50 @@ import java.util.List;
 
 import static com.khebli.gestiondestockproject.utils.Constants.APP_ROOT;
 
+/*
+ * */
+@Api(APP_ROOT + "/article")
 public interface ArticleApi {
-    @PostMapping(value = APP_ROOT+"/article/create",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = APP_ROOT + "/article/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Enregistrer un article(Ajouter /Modifier)", notes = "Cette methode permet d'enregistrer ou modifier un article", response = ArticleDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "L'objet article cree /modifie"),
+            @ApiResponse(code = 400, message = "L'objet article n'est pas validé")
+
+    })
     ArticleDto save(@RequestBody ArticleDto dto);
 
-    @GetMapping(value = APP_ROOT+"/article/{idArticle}",produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = APP_ROOT + "/article/{idArticle}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Rechercher un article", notes = "Cette methode permet chercher un article par son ID", response = ArticleDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "L'Article a été trouvé"),
+            @ApiResponse(code = 404, message = "Aucun Article n'a été trouvé")
+
+    })
     ArticleDto findById(@PathVariable("idArticle") Integer id);
 
-    @GetMapping(value = APP_ROOT+"/article/{codeArticle}",produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = APP_ROOT + "/article/{codeArticle}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Rechercher un article", notes = "Cette methode permet chercher un article par son Code", response = ArticleDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "L'Article à été trouvé"),
+            @ApiResponse(code = 404, message = "Aucun Article n'a été trouvé")
+
+    })
     ArticleDto findByCodeArticle(@PathVariable("codeArticle") String codeArticle);
 
-    @GetMapping(value = APP_ROOT+"/article/all",produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = APP_ROOT + "/article/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Renvoi la liste des articles", notes = "Cette methode permet chercher et envoyer la liste des articles de la BDD",
+            responseContainer = "List<ArticleDto>")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "La liste des articles/ Une liste vide")
+    })
     List<ArticleDto> findAll();
 
-    @DeleteMapping(value = APP_ROOT+"/article/delete/{idArticle}")
+    @DeleteMapping(value = APP_ROOT + "/article/delete/{idArticle}")
+    @ApiOperation(value = "Supprimer un article", notes = "Cette methode permet de supprimer un  article par son ID",
+            response = ArticleDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "L'article à été supprimé avec succes")
+    })
     void delete(@PathVariable("idArticle") Integer id);
 }
