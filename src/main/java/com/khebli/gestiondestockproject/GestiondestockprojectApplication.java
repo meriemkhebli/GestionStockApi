556a2b9142efc89904cfee9f2b66d1307e5b2f8f
meriemkhebli <am_khebli@esi.dz>
2022-09-21 09:10:28 +0100

    version avant securité

--- src/main/java/com/khebli/gestiondestockproject/GestiondestockprojectApplication.java
+++ src/main/java/com/khebli/gestiondestockproject/GestiondestockprojectApplication.java
@@ -2,16 +2,17 @@ package com.khebli.gestiondestockproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //j'ai ajouté @EnableWebMvc apres le déploiement de swagger 2,  un probelem est generé documentationAPi nullpointerexception
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableSwagger2
+
 public class GestiondestockprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestiondestockprojectApplication.class, args);
 	}
+
 
 }
