5f44b4155afd73be6ed0c6452c86c77059f73593
meriemkhebli <am_khebli@esi.dz>
2022-09-21 09:10:08 +0100

    version avant securité

--- src/main/java/com/khebli/gestiondestockproject/handlers/RestExceptionHandler.java
+++ src/main/java/com/khebli/gestiondestockproject/handlers/RestExceptionHandler.java
@@ -1,41 +1,36 @@
 package com.khebli.gestiondestockproject.handlers;
 
 import com.khebli.gestiondestockproject.exception.EntityNotFoundException;
-import com.khebli.gestiondestockproject.exception.ErrorCodes;
 import com.khebli.gestiondestockproject.exception.InvalidEntityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.Collection;
-import java.util.Collections;
 
 @RestControllerAdvice//on a pas besoin restposebody a chaque methode
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorDto> handleException(EntityNotFoundException exception, WebRequest webRequest){
-    final HttpStatus notFound = HttpStatus.NOT_FOUND;
-    final ErrorDto errorDto = ErrorDto.builder()
+        final HttpStatus notFound = HttpStatus.NOT_FOUND;
+        final ErrorDto errorDto = ErrorDto.builder()
                 .code(exception.getErrorCodes())
                 .httpCode(notFound.value())
                 .message(exception.getMessage())
                 .build();
-    return  new ResponseEntity<>(errorDto,notFound);
+        return  new ResponseEntity<>(errorDto,notFound);
     }
 
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorDto> handleException(BadCredentialsException exception, WebRequest webRequest){
+    @ExceptionHandler(InvalidEntityException.class)
+    public ResponseEntity<ErrorDto> handleException(InvalidEntityException exception, WebRequest webRequest){
         final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
         final ErrorDto errorDto = ErrorDto.builder()
-                .code(ErrorCodes.BAD_CREDENTIALS)
+                .code(exception.getErrorCodes())
                 .httpCode(badRequest.value())
                 .message(exception.getMessage())
-                .errors(Collections.singletonList("Login et / ou mot de passe incorrecte"))
+                .errors(exception.getErrors())
                 .build();
         return new ResponseEntity<>(errorDto,badRequest);
 
